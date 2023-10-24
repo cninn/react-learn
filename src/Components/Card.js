cf9522f6a9067c35547c24def5abe0d19f5a4a46
can inan <cninnworks@gmail.com>
2023-10-24 13:52:30 +0300

    beginner

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';//!proptypes importing
 
 //! if not use functional style. you must put this props before.
 class Card extends React.Component {
+
+
+
   render() {
     return (
       <div className="card w-100" style={{ width: "18rem" }}>
@@ -30,6 +33,7 @@ Card.defaultProps = {
 Card.propTypes = {
   cardText: PropTypes.string.isRequired
 }
+
 
 
 export default Card;
